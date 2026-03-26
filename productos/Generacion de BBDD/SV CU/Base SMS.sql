03a3c361466c71bd96273b100dbbcc32839f0feb
Giron Paniagua <72404@bi.com.gt>
2026-03-26 12:50:25 -0600

    nuevo

---
+++ productos/Generacion de BBDD/SV CU/Base SMS.sql
@@ -0,0 +1,85 @@
+/*
+==============================================================================
+Autor: José Antonio Ramírez Recinos - 72927
+Fecha creación / Modificación:
+Descripción:
+==============================================================================
+*/
+
+USE DWHSV;
+
+WITH usuarios_Bi_movil_Salvador as (
+	SELECT * FROM(
+		SELECT
+			  RIGHT('00000000' + RTRIM(LTRIM(CIF)),8) as padded_codigo_cliente,
+			  RTRIM(LTRIM(CIF)) as codigo_cliente,
+			  DW_NOMBRE_OPERADOR as nombre_operador,
+			  Fecha_Crea as fecha_crea,
+			  ROW_NUMBER() OVER(PARTITION BY CIF
+						ORDER BY fecha_crea desc)	AS rn
+		FROM
+			DWHBI.dbo.Dw_sms_Perfil_Usuario
+		WHERE Dw_sms_Perfil_Usuario.DW_DESCRIPCION_STATUS  IN  ( 'ACTIVO'  )
+		AND DW_NOMBRE_OPERADOR  IN  ( 'CLARO SV','Digicel SV','MOVISTAR SV','TIGO SV'  )
+
+	) AS sms
+	WHERE sms.rn = 1
+-- Parámetros para segmentación de clientes
+),
+#seg_clientes as(
+SELECT
+	RTRIM(LTRIM(clientes.cldoc))							AS codigo_cliente,
+	RIGHT('00000000' + RTRIM(LTRIM(clientes.cldoc)), 8)		AS padded_codigo_cliente
+
+FROM
+	his_cif_clientes clientes
+LEFT JOIN
+	dw_bel_ibclie clientes_bel
+	ON RTRIM(LTRIM(clientes.cldoc)) = RTRIM(LTRIM(clientes_bel.clccli))
+LEFT JOIN
+	dw_bel_ibuser usuarios_bel
+	ON RTRIM(LTRIM(clientes.cldoc)) = RTRIM(LTRIM(usuarios_bel.clccli))
+	INNER JOIN 
+	usuarios_Bi_movil_Salvador Bi_movil
+	ON RIGHT('00000000' + RTRIM(LTRIM(clientes.cldoc)), 8) = RTRIM(LTRIM(Bi_movil.padded_codigo_cliente))
+WHERE
+	clientes.dw_fecha_informacion = EOMONTH(DATEADD(MONTH, -1, GETDATE() - 1))
+	AND clientes.estatu = 'A'
+	AND clientes.cltipe = 'N'
+	AND clientes_bel.clccli IS NULL
+	AND usuarios_bel.clccli IS NULL)
+
+-- Información de contacto
+SELECT
+	contacto.codigo_cliente,
+	contacto.nombre_cliente,
+	contacto.correo
+FROM (
+	SELECT
+		seg_clientes.codigo_cliente								AS codigo_cliente,
+		clientes.clnomb											AS nombre_cliente,
+		LOWER(RTRIM(LTRIM(correos.cldire)))						AS correo,
+		ROW_NUMBER() OVER(PARTITION BY seg_clientes.codigo_cliente
+					ORDER BY clientes.clnomb, correos.cldire)	AS rn
+	FROM
+		#seg_clientes seg_clientes
+	INNER JOIN
+		dw_cif_clientes clientes
+		ON seg_clientes.codigo_cliente = RTRIM(LTRIM(clientes.cldoc))
+	INNER JOIN
+		dw_cif_direcciones correos
+		ON seg_clientes.codigo_cliente = RTRIM(LTRIM(correos.cldoc))
+		AND correos.cldico = 4
+		AND correos.cldire LIKE '%_@_%.__%'
+) AS contacto
+WHERE
+	contacto.rn = 1
+	AND  contacto.correo LIKE '%@%.com' -- contiene @ y termina en .com
+	AND LEN( contacto.correo) - LEN(REPLACE( contacto.correo, '@', '')) = 1 -- solo una arroba
+	AND PATINDEX('%[^a-zA-Z0-9@._-]%',  contacto.correo) = 0 -- sin caracteres especiales no permitidos
+	AND  contacto.correo NOT LIKE '%www.%' -- no contiene www.
+	AND CHARINDEX(' ',  contacto.correo) = 0 -- sin espacios
+	AND CHARINDEX('@',  contacto.correo) > 1 -- @ no al inicio
+	AND LEN( contacto.correo) - LEN(REPLACE( contacto.correo, '@', '')) = 1
+
+-- DROP TABLE #seg_clientes;
