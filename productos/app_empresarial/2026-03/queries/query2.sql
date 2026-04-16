d0cc47a345fe5b1961a2a18c3d566992018efbc2
Darturo9 <giron605@gmail.com>
2026-04-16 17:12:52 -0600

    Optimizar query2 de campañas app empresarial

--- productos/app_empresarial/2026-03/queries/query2.sql
+++ productos/app_empresarial/2026-03/queries/query2.sql
@@ -1,17 +1,46 @@
-SELECT RIGHT('00000000' + LEFT(RTRIM(LTRIM(hist_envios.codigo_cliente)),
-				LEN(RTRIM(LTRIM(hist_envios.codigo_cliente))) - 1), 8)		AS padded_codigo_cliente,
-        MIN(month(cmp.start_date ))       as mes,
-	MIN(YEAR(cmp.start_date))		  as año,
-	MIN(cmp.start_date) as fecha
-	FROM
-		dwhbi.dbo.dw_rtm_app_hiscampaignuniverso hist_envios
-	INNER JOIN
-		dwhbi.dbo.dw_rtm_app_campaign cmp
-		ON hist_envios.campaignid = cmp.campaignid
-		AND cmp.start_date >= '2025-05-01'
-		AND cmp.countryid = 3
-		AND cmp.name LIKE '%app empresarial%'
-		AND cmp.name LIKE '%72049%'
-		AND cmp.description LIKE '%reg sqvd%'
-GROUP BY
-hist_envios.codigo_cliente
+WITH campanas_objetivo AS (
+    SELECT
+        c.campaignid,
+        c.start_date
+    FROM dwhbi.dbo.dw_rtm_app_campaign c
+    WHERE c.start_date >= '2025-05-01'
+      AND c.countryid = 3
+      AND c.name LIKE '%app empresarial%'
+      AND c.name LIKE '%72049%'
+      AND CAST(c.description AS NVARCHAR(MAX)) LIKE '%reg sqvd%'
+),
+base AS (
+    SELECT
+        RIGHT('00000000' + y.codigo_sin_dv, 8) AS padded_codigo_cliente,
+        co.start_date
+    FROM dwhbi.dbo.dw_rtm_app_hiscampaignuniverso h
+    INNER JOIN campanas_objetivo co
+        ON co.campaignid = h.campaignid
+    CROSS APPLY (
+        SELECT LTRIM(RTRIM(h.codigo_cliente)) AS codigo_limpio
+    ) x
+    CROSS APPLY (
+        SELECT
+            CASE
+                WHEN LEN(x.codigo_limpio) > 1
+                THEN LEFT(x.codigo_limpio, LEN(x.codigo_limpio) - 1)
+                ELSE NULL
+            END AS codigo_sin_dv
+    ) y
+    WHERE y.codigo_sin_dv IS NOT NULL
+      AND y.codigo_sin_dv <> ''
+),
+primer_envio AS (
+    SELECT
+        padded_codigo_cliente,
+        MIN(start_date) AS fecha
+    FROM base
+    GROUP BY padded_codigo_cliente
+)
+SELECT
+    padded_codigo_cliente,
+    MONTH(fecha) AS mes,
+    YEAR(fecha) AS año,
+    fecha
+FROM primer_envio
+ORDER BY fecha ASC;
